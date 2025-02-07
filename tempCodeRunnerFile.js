88ec6b10bdf66789b55723746cd602efbafd6cce
Avishkar Kanawade <130480745+KanawadeAvi@users.noreply.github.com>
2025-02-07 12:03:28 +0530

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let p = 5;
-
-// for (let i = 0; i <= p; i++) {
-//     let spaces = ' '.repeat(p - i);  // Corrected 't' to 'p'
-//     let stars = '*'.repeat(2 * i - 1);
-//     console.log(spaces + stars + spaces);
-// }
